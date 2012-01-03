59bdafb72627844849abfb0aa6ff283453e4a748
Randy Ortega <ortega571@gmail.com>
2012-01-03 14:03:21 -0430

    añadiendo parche para modificar el idioma del desktop

--- softwarecenter/version.py
+++ softwarecenter/version.py
@@ -1,6 +1,6 @@
 
 import commands
-VERSION='2.0.8-1canaima2'
+VERSION='2.0.8-1canaima3'
 CODENAME=commands.getoutput("lsb_release -s -c")
 DISTRO=commands.getoutput("lsb_release -s -i")
 RELEASE=commands.getoutput("lsb_release -s -r")
